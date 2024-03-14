9f0465da9e7364d17dab4abe6e137ad264eea001
Camille <camille.bedeche@laposte.net>
2024-03-14 17:08:52 +0100

    contenu page principal

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
 <?php
+    include './html/header.html';
+    include './html/nav.php';
+?>
+
+<!-- Corps de la page d'accueil -->
+
+<h1>Bienvenue sur la boutique</h1>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+    aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+
+<?php
+    include './html/footer.html';
+?>
