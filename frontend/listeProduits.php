e12cd8c60b31bc407ea2f4ba94dc3307dd915ea8
Camille <camille.bedeche@laposte.net>
2024-03-15 10:22:06 +0100

    Fonctionnalité ajout au panier

--- frontend/listeProduits.php
+++ frontend/listeProduits.php
@@ -1,25 +1,26 @@
 <div class="row d-flex justify-content-center">
     <div class="col-10 my-5">
         <h1>Liste Produits</h1>
-
+        <div class="d-flex flex-wrap justify-content-center">
         <?php
             require './bdd/produits.php';
-            //var_dump($products);
+            var_dump($products);
 
             foreach ($products as $key => $product) {
                 echo '
-                    <div class="card" style="width: 18rem;">
+                    <div class="card mx-8" style="width: 18rem;">
                        <img src="..." class="card-img-top" alt="...">
                           <div class="card-body">
-                             <h5 class="card-title">' . $products['nom'] . '</h5>
-                                <p class="card-text">' . $products['description'] . '</p>
-                                <p class="card-text">' . $products['prix'] . ' €</p>
-                                <a href="" class="btn btn-primary">Ajouter au panier</a>
+                             <h5 class="card-title">' . $product['nom'] . '</h5>
+                                <p class="card-text">' . $product['description'] . '</p>
+                                <p class="card-text">' . $product['prix'] . ' €</p>
+                                <a href="./backend/addArticle.php?id='. $key .'" class="btn btn-primary">Ajouter au panier</a>
                           </div>
                     </div>
                 ';
             };
         ?>
+        </div>
 
 
 
