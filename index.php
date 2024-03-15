5abdee8d396477eeae51bb67b60b4adb4f545904
Camille <camille.bedeche@laposte.net>
2024-03-15 09:35:28 +0100

    creation du rooter pour naviguer via les liens

--- index.php
+++ index.php
@@ -1,12 +1,34 @@
 <?php
     include './html/header.html';
     include './html/nav.php';
-?>
 
-<!-- Corps de la page d'accueil -->
+    /**
+     * Mise en place du rooter
+     */
+    if($page !== null) {
+        switch ($page) {
+            case 'contact':
+                include './frontend/contact.php';
+                break;
+
+            case 'produits':
+                include './frontend/listeProduits.php';
+                break;
+
+            case 'panier':
+                include './frontend/panier.php';
+                break;
+
+            default:
+                include './frontend/404.html';
+                break;
+        }
+
+    } else {
+        include "./frontend/accueil.html";
+    }
 
 
 
-<?php
     include './html/footer.html';
 ?>
