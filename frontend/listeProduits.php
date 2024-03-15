c163497608fcf811f56cc2a5382fd25644d3488d
Camille <camille.bedeche@laposte.net>
2024-03-15 10:11:05 +0100

    Ajout de la liste des produits sur la page produits

--- frontend/listeProduits.php
+++ frontend/listeProduits.php
@@ -1,9 +1,28 @@
-
-<?php
-?>
 <div class="row d-flex justify-content-center">
     <div class="col-10 my-5">
         <h1>Liste Produits</h1>
+
+        <?php
+            require './bdd/produits.php';
+            //var_dump($products);
+
+            foreach ($products as $key => $product) {
+                echo '
+                    <div class="card" style="width: 18rem;">
+                       <img src="..." class="card-img-top" alt="...">
+                          <div class="card-body">
+                             <h5 class="card-title">' . $products['nom'] . '</h5>
+                                <p class="card-text">' . $products['description'] . '</p>
+                                <p class="card-text">' . $products['prix'] . ' €</p>
+                                <a href="" class="btn btn-primary">Ajouter au panier</a>
+                          </div>
+                    </div>
+                ';
+            };
+        ?>
+
+
+
     </div>
 </div>
 
